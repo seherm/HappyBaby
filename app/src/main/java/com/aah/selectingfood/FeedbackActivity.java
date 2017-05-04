1140f8f5ef2b78535832c2e9ea6746fe5c7df971
manuelschlegel <manuel.schlegel@uzh.ch>
2017-05-05 01:35:14 +0200

    Added a library and some dummies for the Final Feedback. Library: https://github.com/apl-devs/AppIntro

--- app/src/main/java/com/aah/selectingfood/FeedbackActivity.java
+++ app/src/main/java/com/aah/selectingfood/FeedbackActivity.java
@@ -1,31 +1,85 @@
 package com.aah.selectingfood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.support.v4.app.Fragment;
 
-public class FeedbackActivity extends AppCompatActivity {
 
+import android.support.annotation.Nullable;
+import android.graphics.Color;
+import android.widget.Toast;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class FeedbackActivity extends AppIntro {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Add slides
+        addSlide(AppIntroFragment.newInstance("Card 1",
+                "Text Card 1",
+                R.drawable.ic_menu_camera,
+                Color.parseColor("#d4cdc7")));
+        addSlide(AppIntroFragment.newInstance("Card 2",
+                "Text Card 2",
+                R.drawable.ic_menu_share,
+                Color.parseColor("#d4cdc7")));
+
+        // Note here that we DO NOT use setContentView();
+
+
+        // Add your slide fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        /*
+        addSlide(firstFragment);
+        addSlide(secondFragment);
+        addSlide(thirdFragment);
+        addSlide(fourthFragment);
+        */
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+        //addSlide(AppIntroFragment.newInstance(title, description, image, backgroundColor));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#b5aba2"));
+        setSeparatorColor(Color.parseColor("#000000"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(true);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
+        //setVibrate(true);
+        //setVibrateIntensity(30);
     }
 
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
 }
