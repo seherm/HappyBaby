3d3435352db66143e1d734f6eb84943e2983a97b
seherm <sebastian.hermann@hotmail.com>
2017-05-04 14:53:37 +0200

    added possibility to change language in settings (not working yet)

--- app/src/main/java/com/aah/selectingfood/SettingsActivity.java
+++ app/src/main/java/com/aah/selectingfood/SettingsActivity.java
@@ -1,11 +1,16 @@
 package com.aah.selectingfood;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.RadioButton;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -16,6 +21,36 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void changeLanguage(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButtonEnglish:
+                if (checked) {
+                  setLocale("en");
+                }
+                    break;
+            case R.id.radioButtonKhmer:
+                if (checked) {
+                    setLocale("km");
+                }
+                    break;
+        }
+    }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, SettingsActivity.class);
+        startActivity(refresh);
     }
 
 }
