06c934934b51868f5dd67fefdaac5ba70f5c3335
Florian Fuchs <florian.fuchs@bf.uzh.ch>
2017-05-08 11:59:56 +0200

    testing for firstRun, still need to test on real phone

--- app/src/main/java/com/aah/selectingfood/FAQActivity.java
+++ app/src/main/java/com/aah/selectingfood/FAQActivity.java
@@ -1,11 +1,14 @@
 package com.aah.selectingfood;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast; //todo delete line
 
 public class FAQActivity extends AppCompatActivity {
 
@@ -16,5 +19,13 @@ public class FAQActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //TODO delete following part
+        GlobalState state = ((GlobalState) getApplicationContext());
+        Context context = getApplicationContext();;
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, String.valueOf(state.getFirstRun()), duration);
+        toast.show();
     }
 }
