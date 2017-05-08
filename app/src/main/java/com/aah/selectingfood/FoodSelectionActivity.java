c54feb07a1f9c8860999ca30e4b72db70fe7971a
Florian Fuchs <florian.fuchs@bf.uzh.ch>
2017-05-08 15:20:32 +0200

    search functionality step1

--- app/src/main/java/com/aah/selectingfood/FoodSelectionActivity.java
+++ app/src/main/java/com/aah/selectingfood/FoodSelectionActivity.java
@@ -2,10 +2,13 @@ package com.aah.selectingfood;
 
 import com.aah.selectingfood.model.*;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -72,6 +75,8 @@ public class FoodSelectionActivity extends AppCompatActivity {
                 gridViewSelectedFood.setAdapter(selectedFoodAdapter);
             }
         });
+
+        handleIntent(getIntent());
     }
 
     @Override
@@ -80,12 +85,29 @@ public class FoodSelectionActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
 
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
     public void goToFeedbackPage(View view) {
         Intent intent = new Intent(this, FeedbackActivity.class);
         startActivity(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
     }
 }
 
