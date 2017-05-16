06dd48211bbc5d775062632853796a474c8a26e6
seherm <sebastian.hermann@hotmail.com>
2017-05-16 20:08:31 +0200

    removed options menu (settings) on start page (main activity)

--- app/src/main/java/com/aah/selectingfood/activity/MainActivity.java
+++ app/src/main/java/com/aah/selectingfood/activity/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -84,7 +85,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }**/
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
