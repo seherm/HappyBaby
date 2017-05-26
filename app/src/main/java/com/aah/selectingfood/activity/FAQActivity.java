13654a760f12ddec8cf8158064bd7d993aa77361
Florian Fuchs <florian.fuchs@bf.uzh.ch>
2017-05-26 10:22:28 +0200

    small debug stuff

--- app/src/main/java/com/aah/selectingfood/activity/FAQActivity.java
+++ app/src/main/java/com/aah/selectingfood/activity/FAQActivity.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.Toast; //todo delete line
 
 import com.facebook.CallbackManager;
@@ -19,6 +23,11 @@ import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 import com.aah.selectingfood.helper.GlobalState;
 import com.aah.selectingfood.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FAQActivity extends AppCompatActivity {
 
@@ -77,5 +86,23 @@ public class FAQActivity extends AppCompatActivity {
             shareDialog.show(photoContent);
             Log.e("nope","yepyepyep");
         } else Log.e("nope","nope nope nope");*/
+        /*Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));*/
+        /*File file = new File("android.resource://com.aah.selectingfood/" + R.drawable.foodgroup_fruits);
+        MediaScannerConnection.scanFile(
+                getApplicationContext(),
+                new String[]{file.getAbsolutePath()},
+                null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.v("grokkingandroid",
+                                "file " + path + " was scanned seccessfully: " + uri);
+                    }
+                });*/
     }
+
 }
