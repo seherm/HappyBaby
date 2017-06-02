ec9a0c46e1f8e4e7538918300b5fdbfaeb46f91e
seherm <sebastian.hermann@hotmail.com>
2017-06-02 13:24:02 +0200

    removed enum

--- app/src/main/java/com/aah/selectingfood/model/FoodGroup.java
+++
@@ -1,9 +0,0 @@
-package com.aah.selectingfood.model;
-
-/**
- * Created by sebas on 02.06.2017.
- */
-
-public enum FoodGroup {
-    CARBOHYDRATES, FRUITS, LEGUMES,  HERBS, MEAT, VEGETABLES, JUNKFOOD
-}
