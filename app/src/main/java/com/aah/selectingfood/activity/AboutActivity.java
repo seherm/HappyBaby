af6e4482103dccc6f8424ba0b6ba85dec3ba23e2
seherm <sebastian.hermann@hotmail.com>
2017-06-07 19:00:14 +0200

    added open-source licenses dialog

--- app/src/main/java/com/aah/selectingfood/activity/AboutActivity.java
+++ app/src/main/java/com/aah/selectingfood/activity/AboutActivity.java
@@ -3,9 +3,18 @@ package com.aah.selectingfood.activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.aah.selectingfood.R;
+
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.GnuLesserGeneralPublicLicense21;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 
 /** This Activity is for showing static content in an about page**/
 public class AboutActivity extends BaseActivity {
@@ -28,8 +37,34 @@ public class AboutActivity extends BaseActivity {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+
         TextView aboutTextView = (TextView) findViewById(R.id.textViewAbout);
         aboutTextView.setText(versionName);
 
+        ListView aboutListView = (ListView) findViewById(R.id.aboutListView);
+        aboutListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showLicenses();
+            }
+        });
+
+
+
+    }
+
+    public void showLicenses(){
+
+        final Notices notices = new Notices();
+
+        notices.addNotice(new Notice("CircleImageView", "https://github.com/hdodenhof/CircleImageView", "Copyright 2014 - 2017 Henning Dodenhof", new ApacheSoftwareLicense20()));
+        notices.addNotice(new Notice("AppIntro", "https://github.com/apl-devs/AppIntro", "Copyright 2015 Paolo Rotolo\n" + "Copyright 2016 Maximilian Narr", new ApacheSoftwareLicense20()));
+
+        new LicensesDialog.Builder(this)
+                .setTitle("Remarks")
+                .setNotices(notices)
+                .setIncludeOwnLicense(true)
+                .build()
+                .show();
     }
 }
